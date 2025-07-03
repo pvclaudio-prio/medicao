85338553d35865cc2ff69e4e05098d5d5927291d
pvclaudio-prio <cvieira@prio3.com.br>
2025-07-03 15:57:18 -0300

    Update medicoes.py

--- medicoes.py
+++ medicoes.py
@@ -126,10 +126,13 @@ def estruturar_boletim_conciliado(df_boletim_raw: pd.DataFrame, df_contrato: pd.
         (np.round(df_merged["valor_unitario_operacional"], 2) > np.round(df_merged["valor_unitario"], 2))
     ).map({True: "Sim", False: "Não"})
 
-    df_merged["dif_total"] = abs((df_merged["total_recalculado"] - df_merged["total_cobrado"]).fillna(0))
+    df_merged["dif_total"] = (df_merged["total_recalculado"] - df_merged["total_cobrado"]).fillna(0)
     df_merged["flag_total_recalculado_diferente"] = (df_merged["dif_total"] > 1).map({True: "Sim", False: "Não"})
 
-    df_merged["flag_descricao_duplicada"] = df_merged.duplicated(subset=["descricao_completa"], keep=False).map({True: "Sim", False: "Não"})
+    df_merged["flag_descricao_duplicada"] = df_merged.duplicated(
+        subset=["descricao", "descricao_completa"],
+        keep=False
+    ).map({True: "Sim", False: "Não"})
 
     # 🔹 Seleção de colunas finais
     colunas_finais = [
